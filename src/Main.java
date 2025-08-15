617f9431436d8a0c779160f1d5647535583084c3
NeonBite <ivan.duvanov.1@yandex.ru>
2025-08-15 14:07:08 +0700

    feat: making bad code twice again with sonar

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter
+        System.out.println("Enter your name: ");
         String name = sc.nextLine();
 
         System.out.println("Hello, " + name);
